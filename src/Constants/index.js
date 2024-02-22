2b0daabff3b94c113829d21f962ba1308615ba47
SumitJ2002 <sumitjaiswarsj420@gmail.com>
2024-02-22 20:55:17 +0530

    Updated Navigation

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,8 +1,8 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/About', key: 'about', label: 'About Us' },
-    { href: '/about', key: 'about', label: 'For Hospitals' },
-    { href: '/doctorlogin', key: 'doctorlogin', label: 'For Doctors' },
-    { href: '/contact', key: 'contact_us', label: 'Contact' },
-  ];
+  { href: "/", key: "home", label: "Home" },
+  { href: "/About", key: "about", label: "About Us" },
+  { href: "/hospital_login", key: "about", label: "For Hospitals" },
+  { href: "/doctor_login", key: "doctorlogin", label: "For Doctors" },
+  { href: "/contact", key: "contact_us", label: "Contact" },
+];
